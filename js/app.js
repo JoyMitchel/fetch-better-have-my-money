2906dc7171b5982bd875c480f722cecdeb6b3dd9
joy.mitchel@student.moringaschool.com <JoyMitchel>
2025-04-11 16:36:14 +0300

    fetch tasks

--- js/app.js
+++ js/app.js
@@ -1,2 +1,16 @@
 const API_URL = 'http://localhost:3000/todos';
 
+document.addEventListener ('DOMContentLoaded', fetchTasks);
+
+const tasksList = document.querySelector ('#tasks-list');
+
+function fetchTasks() {
+    fetch(API_URL).then(res => {
+        if (!res.ok) {
+            alert (res.status)
+        }
+        
+        return res.json(); 
+    }).then ((todos) => console.log(todos)
+    )
+} 
